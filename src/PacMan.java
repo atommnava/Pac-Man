b3692386559f5857c1cd6a6e210ce0debbdebc9d
fercanalesss <150871769+fercanalesss@users.noreply.github.com>
2024-11-25 21:34:13 -0600

    Imagenes de todos los elementos cargadas

--- src/PacMan.java
+++ src/PacMan.java
@@ -19,7 +19,7 @@ public class PacMan extends JPanel {
         }
     }
     // Mapa
-   private String[] mapa = {
+    private String[] mapa = {
             "XXXXXXXXXXXXXXXXXXX",
             "X          X      X",
             "X XXXX XXXX X XXX X",
@@ -43,6 +43,7 @@ public class PacMan extends JPanel {
             "XXXXXXXXXXXXXXXXXXX",
     };
 
+
     // Medidas
     private int contFilas = 21;
     private int contColumnas = 19;
@@ -50,7 +51,7 @@ public class PacMan extends JPanel {
     private int anchoBorde = contColumnas * marcoTamanio;
     private int largoBorde = contFilas * marcoTamanio;
 
-    // Fantasmas
+    // Imagenes
     private Image muroImagen;
     private Image fantasmaAzul;
     private Image fantasmaRosa;
@@ -77,33 +78,48 @@ public class PacMan extends JPanel {
         // Cargando las imagenes
         pacmanPrincipal = new ImageIcon(getClass().getResource("./pacmanArriba.png")).getImage();
         muroImagen = new ImageIcon(getClass().getResource("./cuadro.png")).getImage();
-
+        fantasmaAzul = new ImageIcon(getClass().getResource("./fantasmaAzul.png")).getImage();
+        fantasmaRosa = new ImageIcon(getClass().getResource("./fantasmaRosa.png")).getImage();
+        fantasmaNaranja = new ImageIcon(getClass().getResource("./fantasmaNaranja.png")).getImage();
+        fantasmaRojo = new ImageIcon(getClass().getResource("./fantasmaRojo.png")).getImage();
         mostrarMapa();
     }
-    public void mostrarMapa(){
-        muros = new HashSet<Bloque>();
-        comidas = new HashSet<Bloque>();
-        fantasmas = new HashSet<Bloque>();
-        for (int i = 0; i < contFilas; i++){
-            for (int j = 0; j < contColumnas; j++){
+
+    public void mostrarMapa() {
+        muros = new HashSet<>();
+        comidas = new HashSet<>();
+        fantasmas = new HashSet<>();
+
+        for (int i = 0; i < contFilas; i++) {
+            for (int j = 0; j < contColumnas; j++) {
                 int x = j * marcoTamanio;
                 int y = i * marcoTamanio;
                 String fila = mapa[i];
                 char mapa = fila.charAt(j);
 
-                //pacman = new Bloque(x,y,marcoTamanio, marcoTamanio, pacmanPrincipal);
                 if (mapa == 'X') {
-                    Bloque muro = new Bloque(x,y,marcoTamanio,marcoTamanio,muroImagen);
+                    Bloque muro = new Bloque(x, y, marcoTamanio, marcoTamanio, muroImagen);
                     muros.add(muro);
                 } else if (mapa == ' ') {
                     Bloque comida = new Bloque(x + 14, y + 14, 4, 4, null);
                     comidas.add(comida);
                 } else if (mapa == 'P') {
-                    pacman = new Bloque(x,y,marcoTamanio, marcoTamanio, pacmanPrincipal);
+                    pacman = new Bloque(x, y, marcoTamanio, marcoTamanio, pacmanPrincipal);
+                } else if (mapa == 'A') {
+                    Bloque fantasmaA = new Bloque(x, y, marcoTamanio, marcoTamanio, fantasmaAzul);
+                    fantasmas.add(fantasmaA);
+                } else if (mapa == 'N') {
+                    Bloque fantasmaN = new Bloque(x, y, marcoTamanio, marcoTamanio, fantasmaNaranja);
+                    fantasmas.add(fantasmaN);
+                } else if (mapa == 'R') {
+                    Bloque fantasmaR = new Bloque(x, y, marcoTamanio, marcoTamanio, fantasmaRosa);
+                    fantasmas.add(fantasmaR);
+                } else if (mapa == 'J') {
+                    Bloque fantasmaJ = new Bloque(x, y, marcoTamanio, marcoTamanio, fantasmaRojo);
+                    fantasmas.add(fantasmaJ);
                 }
             }
         }
-
     }
 
     @Override
@@ -112,12 +128,14 @@ public class PacMan extends JPanel {
         g.setColor(Color.yellow);
         draw(g);
     }
-    public void draw(Graphics g){
+
+    public void draw(Graphics g) {
         g.drawImage(pacman.imagen, pacman.x, pacman.y, pacman.ancho, pacman.largo, null);
         for (Bloque muro : muros) {
             g.drawImage(muro.imagen, muro.x, muro.y, muro.ancho, muro.largo, null);
         }
+        for (Bloque fantasma : fantasmas) {
+            g.drawImage(fantasma.imagen, fantasma.x, fantasma.y, fantasma.ancho, fantasma.largo, null);
+        }
     }
 }
-
-
