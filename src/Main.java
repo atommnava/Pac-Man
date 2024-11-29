30a01df5760f01a3ff109613adcb922e7eeebf2a
fercanalesss <150871769+fercanalesss@users.noreply.github.com>
2024-11-28 22:56:40 -0600

    Comentarios añadidos

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,31 @@
-import javax.swing.*;
+import javax.swing.*; //Biblioteca gráfica
 
 public class Main {
     public static void main(String[] args) {
-        int contFilas = 21;
-        int contColumnas = 19;
-        int marcoTamanio = 32; // N pixeles que abarca
-        int anchoBorde = contColumnas * marcoTamanio;
-        int largoBorde = contFilas * marcoTamanio;
+
+        //Área visible en la ventana (mapa)
+        int contFilas = 21; //Filas
+        int contColumnas = 19; //Columnas
+        int marcoTamanio = 32; // Tamaño de pixeles que abarca cada celda
+        int anchoBorde = contColumnas * marcoTamanio; //Ancho total
+        int largoBorde = contFilas * marcoTamanio; //Largo total
+
+        //Creación de ventana con el título "Pac Man"
         JFrame frame = new JFrame("Pac Man");
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false); // El usaurio no podrá ampliar la ventana
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // 600 * 700 px
+
+        //Configuración de la ventana
+        frame.setLocationRelativeTo(null); //Coloca la ventana en el centro de la pantalla
+        frame.setResizable(false); // Impide que el usaurio no pueda ampliar la ventana
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Al cerrar la ventana, el programa se detiene
+
+        //Define el tamaño de la ventana con lo calculado: 600 * 700 px
         frame.setSize(anchoBorde, largoBorde);
 
+        //Creación de objeto PacMan
         PacMan pacman = new PacMan();
-        frame.add(pacman);
-        frame.pack();
-        pacman.requestFocus();
-        frame.setVisible(true);
+        frame.add(pacman); //Se agrega a la ventana como componente visual
+        frame.pack(); //Asegura que la ventana se ajuste a sus medidas
+        pacman.requestFocus(); //Permite que pacman pueda recibir órdenes de teclado
+        frame.setVisible(true); //Hacer visible la ventana
     }
 }
